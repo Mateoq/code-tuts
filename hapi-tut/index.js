99fcb700e898994fb00714b1b1740f00fa9098b3
Mateo <mquintero@hugeinc.com>
2017-02-06 17:02:55 -0500

    Hapijs Tut: Replying resquests

--- hapi-tut/index.js
+++ hapi-tut/index.js
@@ -26,44 +26,43 @@ server.connection({
 server.register({
   register: Good,
   options: goodOptions
-}, (err) => {
-  server.route({
-    method: 'GET',
-    path: '/',
-    handler(request, reply) {
-      reply('hello hapi');
-    }
-  });
-
-  server.route({
-    method: 'GET',
-    path: '/{name}',
-    handler(request, reply) {
-      reply(`Hello ${request.params.name}`);
-    }
-  });
-
-  // Url param optional
-  server.route({
-    method: 'GET',
-    path: '/users/{userId?}',
-    handler: (request, reply) => {
-      reply(request.params);
-    }
-  });
-
-  // Url param wild card
-  // e.g. http://localhost/files/a/b/c.png
-  server.route({
-    method: 'GET',
-    path: '/files/{file*}',
-    handler: (request, reply) => {
-      reply(request.params);
-    }
-  });
-
-  server.start(() => {
-    console.log(`Started at: ${server.info.uri}`);
-  });
 });
 
+server.route({
+  method: 'GET',
+  path: '/',
+  handler(request, reply) {
+    reply('hello hapi');
+  }
+});
+
+server.route({
+  method: 'GET',
+  path: '/{name}',
+  handler(request, reply) {
+    reply(`Hello ${request.params.name}`);
+  }
+});
+
+// Url param optional
+server.route({
+  method: 'GET',
+  path: '/users/{userId?}',
+  handler: (request, reply) => {
+    reply(request.params);
+  }
+});
+
+// Url param wild card
+// e.g. http://localhost/files/a/b/c.png
+server.route({
+  method: 'GET',
+  path: '/files/{file*}',
+  handler: (request, reply) => {
+    reply(request.params);
+  }
+});
+
+server.start(() => {
+  console.log(`Started at: ${server.info.uri}`);
+});
